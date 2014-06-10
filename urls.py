765f9eb5ea7a6de9bda1dd241c1c0904f09aead8
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-06-10 10:26:25 +0530

    Yogesh || Refactored url pattern to be restful and changed the respective handlers

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-from datawinners.dcs_app.view import get_questions, get_question, submission_handler, get_submission, submit_submission
+from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update
 
 urlpatterns = patterns('',
     url(r'^client/project/$', get_questions),
-    url(r'^client/project/(?P<project_uuid>\w+?)/submission/(?P<submission_uuid>\w+?)$', get_submission),
-    url(r'^client/project/(?P<project_uuid>\w+?)/submission/', submission_handler), # on POST submit_submission
+    url(r'^client/project/(?P<project_uuid>\w+?)/submission/(?P<submission_uuid>\w+?)$', submission_get_or_update),
+    url(r'^client/project/(?P<project_uuid>\w+?)/submission/', all_submissions_or_new),
     url(r'^client/project/(?P<project_uuid>\w+?)$', get_question),
 )
