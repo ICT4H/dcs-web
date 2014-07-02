727b3087c8d4ba8253a50ae22b52fd72ed410158
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-07-02 14:25:53 +0530

    Yogesh || Added auth api

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
-from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update
+from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update, authenticate_user
 
 urlpatterns = patterns('',
     url(r'^client/project/$', get_questions),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/(?P<submission_uuid>\w+?)$', submission_get_or_update),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/', all_submissions_or_new),
     url(r'^client/project/(?P<project_uuid>\w+?)$', get_question),
+    url(r'^client/auth/$', authenticate_user),
+
 )
