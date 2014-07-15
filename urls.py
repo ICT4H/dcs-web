0270ccec098957974dc5e5a6d33b92c1b1a49eaa
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-07-15 15:24:42 +0530

    Yogesh || Fixed basic auth picking user from session and submission pagination api

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update, authenticate_user
+from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update, authenticate_user, get_server_submissions, get_submission_headers
 
 urlpatterns = patterns('',
     url(r'^client/project/$', get_questions),
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/', all_submissions_or_new),
     url(r'^client/project/(?P<project_uuid>\w+?)$', get_question),
     url(r'^client/auth/$', authenticate_user),
+    url(r'^client/submissions/$', get_server_submissions),
+    url(r'^client/submission-headers/$', get_submission_headers),
 
 )
