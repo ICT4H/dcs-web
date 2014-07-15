e67c5728883246882ea86ae9a4365e808cd22fed
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-07-15 15:24:42 +0530

    Yogesh || Fixed basic auth picking user from session and submission pagination api

---
+++ Submission.py
@@ -0,0 +1,78 @@
+from django.utils.translation import get_language, ugettext
+from datawinners.search.index_utils import es_field_name, es_unique_id_code_field_name
+from datawinners.search.query import Query
+from datawinners.search.submission_headers import HeaderFactory
+from datawinners.search.submission_index_constants import SubmissionIndexConstants
+from datawinners.search.submission_query import SubmissionQueryBuilder
+
+
+class SubmissionQueryMobile(Query):
+    SUBMISSION_TYPE = "mobile"
+    def __init__(self, form_model, query_params):
+        Query.__init__(self, SubmissionQueryMobileResponseCreator(form_model), SubmissionQueryBuilder(form_model),
+                       query_params)
+        self.form_model = form_model
+
+    def get_headers(self, entity_type, user):
+        headers = self.query_params['headers'] if self.query_params.get('headers', None) else \
+            HeaderFactory(self.form_model).create_header(self.SUBMISSION_TYPE).get_header_field_names()
+        return headers
+
+    def add_unique_id_field(self, unique_id_field, header_dict):
+        return None
+
+    def get_header_dict(self):
+        header = HeaderFactory(self.form_model).create_header(self.SUBMISSION_TYPE)
+        #{k,v for k1,v1 in header}
+
+        return header.get_header_field_dict()
+
+    def query(self, database_name):
+        query_all_results = self.query_builder.query_all(database_name, self.form_model.id)
+
+        # take all headers if nothing selected
+
+        # headers = ["4934e8e8072d11e4ae2b001c42af7554_your_name"]#self.query_params.get('headers')
+        headers = HeaderFactory(self.form_model).create_header(self.SUBMISSION_TYPE)
+        # submission_headers = header.get_header_field_names()
+        submission_headers = headers
+        query_by_submission_type = self.query_builder.filter_by_submission_type(query_all_results, self.query_params)
+        filtered_query = self.query_builder.add_query_criteria(submission_headers, query_by_submission_type,
+                                                               self.query_params.get('search_filters').get(
+                                                                   'search_text'),
+                                                               query_params=self.query_params)
+        submissions = self.response_creator.create_response(submission_headers, filtered_query)
+        return submissions
+
+class SubmissionQueryMobileResponseCreator():
+    def __init__(self, form_model):
+        self.form_model = form_model
+
+    def create_response(self, required_field_names, query):
+        entity_question_codes = [es_field_name(field.code, self.form_model.id) for field in
+                                 self.form_model.entity_questions]
+        meta_fields = [SubmissionIndexConstants.DATASENDER_ID_KEY]
+        meta_fields.extend([es_unique_id_code_field_name(code) for code in entity_question_codes])
+
+        submissions = []
+        language = get_language()
+        for res in query.values_dict(tuple(required_field_names)):
+            submission = [res._id]
+            for key in required_field_names:
+                if not key in meta_fields:
+                    #TODO do we need entity_question
+                    if key in entity_question_codes:
+                        self.combine_name_and_id(short_code=res.get(es_unique_id_code_field_name(key)),
+                                                 entity_name=res.get(key), submission=submission)
+                    elif key == 'status':
+                        submission.append(ugettext(res.get(key)))
+
+                    elif key == 'error_msg':
+                        error_msg = res.get(key)
+                        if error_msg.find('| |') != -1:
+                            error_msg = error_msg.split('| |,')[['en', 'fr'].index(language)]
+                        submission.append(error_msg)
+                    else:
+                        submission.append(res.get(ugettext(key)))
+            submissions.append(submission)
+        return submissions
--- view.py
+++ view.py
@@ -1,18 +1,25 @@
 import logging
 import re
-from django.http import HttpResponseBadRequest, HttpResponseNotFound
+import json
+
+from django.contrib.auth.models import User
+from django.http import HttpResponseBadRequest, HttpResponseNotFound, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from datawinners.blue.auth import logged_in_or_basicauth, response_json_cors, enable_cors
+import jsonpickle
+
+from datawinners.dcs_app.auth import basicauth_allow_cors, response_json_cors, enable_cors
 from datawinners.blue.view import SurveyWebXformQuestionnaireRequest, logger
 from datawinners.blue.xform_bridge import XFormTransformer, XFormSubmissionProcessor
 from datawinners.blue.xform_web_submission_handler import XFormWebSubmissionHandler
+from datawinners.dcs_app.Submission import SubmissionQueryMobile
 from datawinners.main.database import get_database_manager
 from mangrove.form_model.form_model import FormModel
+
 
 logger = logging.getLogger("datawinners.xlfrom.client")
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def get_questions(request):
     manager =  get_database_manager(request.user)
     project_list = []
@@ -25,12 +32,12 @@ def get_questions(request):
     return response_json_cors(project_list)
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def authenticate_user(request):
     return response_json_cors({'auth':'success'})
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def get_question(request, project_uuid):
     manager = get_database_manager(request.user)
     questionnaire = FormModel.get(manager, project_uuid)
@@ -39,7 +46,7 @@ def get_question(request, project_uuid):
 
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def all_submissions_or_new(request, project_uuid):
     if request.method == 'GET':
         survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
@@ -57,7 +64,7 @@ def all_submissions_or_new(request, project_uuid):
 
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def submission_get_or_update(request, project_uuid, submission_uuid):
     if request.method == 'GET':
         survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
@@ -75,7 +82,7 @@ def submission_get_or_update(request, project_uuid, submission_uuid):
             return HttpResponseBadRequest()
 
 @csrf_exempt
-@logged_in_or_basicauth()
+@basicauth_allow_cors()
 def submit_submission(request):
     try:
         response = XFormWebSubmissionHandler(request.user, request=request).\
@@ -85,3 +92,70 @@ def submit_submission(request):
     except Exception as e:
         logger.exception("Exception in submission : \n%s" % e)
         return HttpResponseBadRequest()
+
+def get_submission_headers(request):
+    user = User.objects.get(username='tester150411@gmail.com')
+    # NGOUserProfile.objects.filter(user=user)
+    dbm = get_database_manager(user)
+
+    form_model = FormModel.get(dbm, request.GET.get('uuid'))
+
+    # {"submissionDatePicker":"All Dates","datasenderFilter":"","search_text":"","dateQuestionFilters":{},"uniqueIdFilters":{}}
+    headers = SubmissionQueryMobile(form_model, query_params=None).get_header_dict()
+
+    return response_json_cors({
+                'data': headers
+            })
+
+# @csrf_exempt
+# @logged_in_or_basicauth()
+def get_server_submissions(request):
+    user = User.objects.get(username='tester150411@gmail.com')
+    # NGOUserProfile.objects.filter(user=user)
+    dbm = get_database_manager(user)
+
+
+    form_model = FormModel.get(dbm, request.GET.get('uuid'))
+
+    search_parameters = {}
+    # 4934e8e8072d11e4ae2b001c42af7554_your_name
+
+    start = int(request.GET.get('start', '0'))
+    search_parameters.update({"start_result_number": start})
+    length = int(request.GET.get('length', '10'))
+
+    search_parameters.update({"number_of_results": length})
+    search_parameters.update({"filter":'all'})
+
+    search_parameters.update({"sort_field": "4934e8e8072d11e4ae2b001c42af7554_your_name"})
+    search_parameters.update({"order": ""})
+    # search_parameters.update({"headers": json.loads('["c3b9ac7c07f811e4a302001c42af7554_q2"]')})
+
+
+    search_filters = json.loads('{"submissionDatePicker":"All Dates","datasenderFilter":"","search_text":"","dateQuestionFilters":{},"uniqueIdFilters":{}}')
+
+    # {"submissionDatePicker":"All Dates","datasenderFilter":"","search_text":"","dateQuestionFilters":{},"uniqueIdFilters":{}}
+    search_parameters.update({"search_filters": search_filters})
+    search_text = search_filters.get("search_text", '')
+    search_parameters.update({"search_text": search_text})
+
+    submission_query = SubmissionQueryMobile(form_model, search_parameters)
+    header_dict = submission_query.get_header_dict()
+    query_count, search_count, submissions = submission_query.paginated_query(user, form_model.id)
+
+    return enable_cors(HttpResponse(
+        jsonpickle.encode(
+            {
+                'data': submissions,
+                'headers': header_dict.values(),
+                'total': query_count,
+                'start': start,
+                "search_count": search_count,
+                'length': length
+            }, unpicklable=False), content_type='application/json'))
+# type=all
+# sEcho=8 iColumns=6 sColumns= iDisplayStart=0 iDisplayLength=25 sSearch= bRegex=false sSearch_0= bRegex_0=false bSearchable_0=true
+# sSearch_1= bRegex_1=false bSearchable_1=true sSearch_2= bRegex_2=false bSearchable_2=true sSearch_3= bRegex_3=false bSearchable_3=true
+# sSearch_4= bRegex_4=false bSearchable_4=true sSearch_5= bRegex_5=false bSearchable_5=true iSortingCols=1 iSortCol_0=2 sSortDir_0=desc
+# bSortable_0=false bSortable_1=true bSortable_2=true bSortable_3=true bSortable_4=true bSortable_5=true disable_cache=1404814386137
+# search_filters={"submissionDatePicker":"All Dates","datasenderFilter":"","search_text":"ssdfsdf","dateQuestionFilters":{},"uniqueIdFilters":{}}
