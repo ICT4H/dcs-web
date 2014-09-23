2dca2a5136cf90ce05b1e098d24cc55447cdf4e8
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-09-23 11:46:29 +0530

    Yogesh || Including dcs_app urls

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update, authenticate_user, get_server_submissions, get_submission_headers, check_submissions_status, \
     get_projects_status
 
 urlpatterns = patterns('',
+    (r'', include('datawinners.urls')), #including the datawinners url
+
     url(r'^client/project/$', get_questions),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/check-status', check_submissions_status),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/(?P<submission_uuid>\w+?)$', submission_get_or_update),
