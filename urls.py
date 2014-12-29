d22f42d3df480e54fbf0828549116e7a5d20d46b
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-12-29 16:35:14 +0530

    Yogesh || project and submission to support bulk download

--- urls.py
+++ urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import patterns, url, include
-from datawinners.dcs_app.view import get_questions, get_question, all_submissions_or_new, submission_get_or_update, authenticate_user, get_server_submissions, get_submission_headers, check_submissions_status, \
+from datawinners.dcs_app.view import get_questions_paginated_or_by_ids, get_question, all_submissions_or_by_ids_get_or_new_post, submission_get_or_update, authenticate_user, get_server_submissions, get_submission_headers, check_submissions_status, \
     get_projects_status, attachment_post, attachment_get, get_delta_submission
     
 urlpatterns = patterns('',
     (r'', include('datawinners.urls')), #including the datawinners url
     (r'', include('datawinners.dcs_web.urls')),
 
-    url(r'^client/project/$', get_questions),
+    url(r'^client/project/$', get_questions_paginated_or_by_ids),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/check-status', check_submissions_status),
     url(r'^client/project/(?P<project_uuid>\w+?)/submission/(?P<submission_uuid>\w+?)$', submission_get_or_update),
-    url(r'^client/project/(?P<project_uuid>\w+?)/submission/', all_submissions_or_new),
+    url(r'^client/project/(?P<project_uuid>\w+?)/submission/', all_submissions_or_by_ids_get_or_new_post),
 
     url(r'^client/project/(?P<project_uuid>\w+?)$', get_question),
     url(r'^client/auth/$', authenticate_user),
