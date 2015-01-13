f08955b43d4400534fedc3a2ec8470c1feefa86d
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2015-01-13 17:17:53 +0530

    Yogesh || Refactored the api urls

--- view.py
+++ view.py
@@ -9,8 +9,7 @@ from django.http import HttpResponseBadRequest, HttpResponseNotFound, HttpRespon
 from django.views.decorators.csrf import csrf_exempt
 import magic
 
-from datawinners.project.submission.submission_search import get_submissions_paginated, get_submission_count, \
-    get_submissions_without_user_filters_count
+from datawinners.project.submission.submission_search import get_submissions_paginated, get_submission_count
 from datawinners.accountmanagement.localized_time import get_country_time_delta
 from datawinners.dataextraction.helper import convert_date_string_to_UTC
 from datawinners.dcs_app.auth import basicauth_allow_cors, response_json_cors, enable_cors
@@ -98,46 +97,64 @@ def check_submissions_status(request, project_uuid):
         'server-deleted':ids_not_found,
         'outdated':outdated_ids})
 
+
 @csrf_exempt
 @basicauth_allow_cors()
-def all_submissions_or_by_ids_get_or_new_post(request, project_uuid):
+def paginated_submissions_or_by_id_or_create(request, project_uuid):
     if request.method == 'GET':
-        ids = request.GET.getlist('ids')
-        if ids:
-            survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
-            submissions = survey_request.get_many_submissions(ids)
-            return response_json_cors(submissions)
-        else:
-            start = int(request.GET.get('start', '0'))
-            page_size = int(request.GET.get('length', '10'))
-            survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
-            total_count, slim_submissions = survey_request.get_submissions(start, page_size)
-            return response_json_cors(slim_submissions)
+        return _paginate_submissions_or_by_ids(project_uuid, request)
 
     elif request.method == 'POST':
-        try:
-            form_code = get_form_code_from_xform(request.POST['form_data']);
-            response = XFormWebSubmissionHandler(request=request, form_code=form_code).\
-                create_new_submission_response()
-            return enable_cors(response)
-        except Exception as e:
-            logger.exception("Exception in submission : \n%s" % e)
-            return HttpResponseBadRequest()
+        return _create_submission(request)
 
-def get_form_code_from_xform(xform):
+def _get_submissions_by_ids(project_uuid, request):
+    survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
+    submissions = survey_request.get_many_submissions(request.GET['ids'])
+    return response_json_cors(submissions)
+
+def _paginate_submissions_or_by_ids(project_uuid, request):
+
+    if request.GET.getlist('ids'):
+        return _get_submissions_by_ids(project_uuid, request)
+    else:
+        is_slim_submission_requested = not (request.GET.get('view') and request.GET['view'] == 'full')
+        survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
+        slim_submission_response = _get_slim_submission_paginated(request, project_uuid)
+
+        if is_slim_submission_requested:
+            return response_json_cors(slim_submission_response)
+
+        ID_INDEX = 0
+        submission_ids = [slim_submission[ID_INDEX] for slim_submission in slim_submission_response['data']]
+        submissions_response = survey_request.get_many_submissions(submission_ids)
+        submissions_response_paginated = slim_submission_response
+        submissions_response_paginated.update({'data': submissions_response})
+        return response_json_cors(submissions_response_paginated)
+
+def _create_submission(request):
+    try:
+        form_code = _get_form_code_from_xform(request.POST['form_data']);
+        response = XFormWebSubmissionHandler(request=request, form_code=form_code). \
+            create_new_submission_response()
+        return enable_cors(response)
+    except Exception as e:
+        logger.exception("Exception in submission : \n%s" % e)
+        return HttpResponseBadRequest()
+
+def _get_form_code_from_xform(xform):
     return re.search('<form_code>(.+?)</form_code>', xform).group(1)
 
 
 @csrf_exempt
 @basicauth_allow_cors()
-def submission_update(request, project_uuid, submission_uuid):
+def update_submission_or_get_by_id(request, project_uuid, submission_uuid):
     if request.method == 'GET':
         survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
         content = survey_request.get_submission(submission_uuid)
         return response_json_cors(content)
     elif request.method == 'POST':
         try:
-            form_code = get_form_code_from_xform(request.POST['form_data']);
+            form_code = _get_form_code_from_xform(request.POST['form_data']);
             response = XFormWebSubmissionHandler(request=request, form_code=form_code).\
                 update_submission_response(submission_uuid)
             return enable_cors(response)
@@ -196,7 +213,7 @@ def attachment_get(request, survey_response_id, file_name):
 @csrf_exempt
 @basicauth_allow_cors()
 def get_delta_submission(request, project_uuid):
-    survey_request = SurveyWebXformQuestionnaireRequest(request, request.GET.get('uuid'), XFormSubmissionProcessor())
+    survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
     to_time = convert_date_string_to_UTC(datetime.now().strftime("%d-%m-%Y"), datetime.now().time().strftime("%H:%M:%S"))
     from_time = convert_date_string_to_UTC(request.GET.get('last_fetch'))
     content = survey_request.get_submission_from(from_time, to_time)
@@ -205,9 +222,9 @@ def get_delta_submission(request, project_uuid):
                                'last_fetch': datetime.now().strftime("%d-%m-%Y")})
 
 
-def _get_slim_submission_paginated(request):
+def _get_slim_submission_paginated(request, project_uuid):
     dbm = get_database_manager(request.user)
-    form_model = FormModel.get(dbm, request.GET.get('uuid'))
+    form_model = FormModel.get(dbm, project_uuid)
     length = int(request.GET.get('length', '10'))
     start = int(request.GET.get('start', '0'))
     search_parameters = {}
@@ -227,9 +244,8 @@ def _get_slim_submission_paginated(request):
     local_time_delta = get_country_time_delta(organization.country)
     search_results, query_fields = get_submissions_paginated(dbm, form_model, search_parameters, local_time_delta)
     submission_count_with_filters = get_submission_count(dbm, form_model, search_parameters, local_time_delta)
-    submission_count_without_filters = get_submissions_without_user_filters_count(dbm, form_model, search_parameters)
     submissions = SubmissionQueryResponseCreator(form_model, local_time_delta) \
-        .create_response(query_fields, search_results)
+                    .create_response(query_fields, search_results)
     return {
         'data': submissions,
         'headers': '',
@@ -238,28 +254,3 @@ def _get_slim_submission_paginated(request):
         "search_count": len(submissions),
         'length': length
     }
-
-def _get_slim_submissions(request):
-    return enable_cors(HttpResponse(
-        json.dumps(_get_slim_submission_paginated(request)),
-                    content_type='application/json'))
-
-@csrf_exempt
-@basicauth_allow_cors()
-def get_server_submissions(request):
-    if request.method == 'GET':
-        is_full_submission_requested = request.GET.get('view') and request.GET.get('view') == 'full'
-        if is_full_submission_requested:
-            return _get_full_submissions_paginated(request, request.GET.get('uuid'))
-        else:
-            return _get_slim_submissions(request)
-
-def _get_full_submissions_paginated(request, project_uuid):
-    slim_submission_paginated = _get_slim_submission_paginated(request)
-    ID_INDEX = 0
-    submission_ids = [slim_submission[ID_INDEX] for slim_submission in slim_submission_paginated['data']]
-    survey_request = SurveyWebXformQuestionnaireRequest(request, project_uuid, XFormSubmissionProcessor())
-    submissions_response = survey_request.get_many_submissions(submission_ids)
-    submissions_response_paginated = slim_submission_paginated
-    submissions_response_paginated.update({'data': submissions_response})
-    return response_json_cors(submissions_response_paginated)
