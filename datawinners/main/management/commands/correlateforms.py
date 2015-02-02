d2705b6ebcfcbe4331080afa3438730efc9bf5b7
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2015-02-02 12:04:30 +0530

    Yogesh || Added command to correlate project. This will be removed once UI is build.

---
+++ datawinners/main/management/commands/correlateforms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from datawinners.blue.correlated_xlxform import CorrelatedForms
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        if len(args) != 2:
+            print "Please provide parent and child project uuids"
+            return
+
+        self.parent_uuid = args[0]
+        self.child_uuid = args[1]
+        user_name = args[2] if len(args) > 2 else 'tester150411@gmail.com'
+
+        self.user = User.objects.get(username=user_name)
+        self.new_child_action_label= 'Repayment'
+        print "Trying to correlate parent: %s; child: %s" %(self.parent_uuid, self.child_uuid)
+        self._correlate_projects()
+
+    def _correlate_projects(self):
+        correlated_forms = CorrelatedForms(self.user)
+        if correlated_forms.relate_forms(self.parent_uuid, self.child_uuid, self.new_child_action_label):
+            print('Project related successfully')
+        else:
+            print('Failed to relate project')
