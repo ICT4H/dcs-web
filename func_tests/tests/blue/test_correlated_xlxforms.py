c0d951cf451ccd46e5934d0fe1cd86e704244428
yogeshsr <yogeshsr@thoughtworks.com>
2015-03-05 15:39:59 +0530

    Yogesh || Initial commit- UI for correlated forms

--- func_tests/tests/blue/test_correlated_xlxforms.py
+++ func_tests/tests/blue/test_correlated_xlxforms.py
@@ -5,10 +5,12 @@ import time
 
 from django.contrib.auth.models import User
 
-from datawinners.blue.correlated_xlxform import CorrelatedForms, NoCommonFieldsException, MultipleChildrenNotSupported, \
+from datawinners.alldata.helper import get_all_project_for_user
+from datawinners.blue.correlated_xlxform import CorrelatedForms, NoCommonFieldsException, \
     ParentProjectWithFieldSetNotSupported, ParentXform
 from datawinners.blue.xform_bridge import XlsFormParser, MangroveService
 from datawinners.main.database import get_database_manager
+from datawinners.project import helper
 from mangrove.form_model.project import Project
 
 
@@ -28,13 +30,26 @@ class TestCorrelatedXlsForms(unittest.TestCase):
         self.dbm = get_database_manager(self.user)
         self.random_project_name = str(time.time())
         #Do this before creating projects for this test suite
-        self.repayment_project_id = self._create_test_projects('Repayment-' + self.random_project_name, self.REPAYMENT)
-        self.loan_account_id = self._create_test_projects('Loan account-'+ self.random_project_name, self.LOAN_ACCOUNT)
+        self.clean_projects = set()
+        self.repayment_project_id = self._create_test_projects_and_delete_teardown('Repayment-' + self.random_project_name, self.REPAYMENT)
+        self.loan_account_id = self._create_test_projects_and_delete_teardown('Loan account-'+ self.random_project_name, self.LOAN_ACCOUNT)
+
+    def tearDown(self):
+        self._delete_all_projects_created_by_this_test_run()
+
+    def _delete_all_projects_created_by_this_test_run(self):
+        questionnaires = get_all_project_for_user(self.user)
+        ids = [q['value']['_id'] for q in questionnaires if q['value']['_id'] in self.clean_projects]
+        [self._del_project(Project.get(self.dbm, id)) for id in ids]
+
+    def _del_project(self, project):
+        helper.delete_project(project)
+        return project.delete()
 
     def test_should_add_parent_info_to_child_questionnaire(self):
         correlated_forms = CorrelatedForms(self.user)
 
-        correlated_forms.relate_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
+        correlated_forms.relate_parent_and_child_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
 
         updated_child_project = Project.get(self.dbm, self.repayment_project_id)
         self._asset_parent_fields_code_label(updated_child_project)
@@ -50,47 +65,52 @@ class TestCorrelatedXlsForms(unittest.TestCase):
 
     def test_should_verify_parent_and_child_project_has_at_least_one_common_field_to_establish_relation(self):
         correlated_forms = CorrelatedForms(self.user)
-        project_with_no_matching_fields_with_loan_account = self._create_test_projects('no-matching-fields-'
+        project_with_no_matching_fields_with_loan_account = self._create_test_projects_and_delete_teardown('no-matching-fields-'
                                                                                + self.random_project_name, self.NO_MATCHING_FIELDS)
         with self.assertRaises(NoCommonFieldsException):
-            correlated_forms.relate_forms(self.loan_account_id, project_with_no_matching_fields_with_loan_account,
+            correlated_forms.relate_parent_and_child_forms(self.loan_account_id, project_with_no_matching_fields_with_loan_account,
                                           'New Child')
 
     def test_should_add_child_id_to_parent_questionnaire(self):
         correlated_forms = CorrelatedForms(self.user)
 
-        correlated_forms.relate_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
+        correlated_forms.relate_parent_and_child_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
 
         updated_parent_project = Project.get(self.dbm, self.loan_account_id)
         self.assertIn(self.repayment_project_id, updated_parent_project.child_ids)
         self.assertTrue(updated_parent_project.is_parent_project)
 
-    #TODO remove when multiple children support is build
-    def test_should_not_allow_to_have_multiple_children(self):
+    def test_should_verify_child_has_only_the_recent_parent_and_old_parent_do_not_have_child(self):
         correlated_forms = CorrelatedForms(self.user)
-        correlated_forms.relate_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
+        correlated_forms.relate_parent_and_child_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
+        new_loan_form_id = self._create_test_projects_and_delete_teardown('New Loan account-'+ self.random_project_name, self.LOAN_ACCOUNT)
 
-        with self.assertRaises(MultipleChildrenNotSupported):
-            correlated_forms.relate_forms(self.loan_account_id, self.repayment_project_id, 'Repayment')
+        correlated_forms.relate_parent_and_child_forms(new_loan_form_id, self.repayment_project_id, 'Repayment')
+
+        updated_child_project = Project.get(self.dbm, self.repayment_project_id)
+        self.assertEqual(updated_child_project.parent_info.get("parent_uuid"), new_loan_form_id)
+
+        prev_parent_project = Project.get(self.dbm, self.loan_account_id)
+        self.assertNotIn(self.repayment_project_id, prev_parent_project.child_ids)
 
     def test_should_hide_common_parent_fields_in_child_xform_when_accessed_by_dcs_apis(self):
         # This needs to be done only for dcs app. For odk and web the parent fields remain editable.
         pass
 
     def test_should_not_allow_parent_to_contain_field_set_field_to_support_excel_import(self):
-        project_with_repeat_field = self._create_test_projects('Project-with-repeat-'
+        project_with_repeat_field = self._create_test_projects_and_delete_teardown('Project-with-repeat-'
                                                                                + self.random_project_name, self.REPEAT)
         correlated_forms = CorrelatedForms(self.user)
         with self.assertRaises(ParentProjectWithFieldSetNotSupported):
-            correlated_forms.relate_forms(project_with_repeat_field, self.repayment_project_id, 'Repayment')
+            correlated_forms.relate_parent_and_child_forms(project_with_repeat_field, self.repayment_project_id, 'Repayment')
 
     def test_should_relate_child_containing_field_set_question(self):
         correlated_forms = CorrelatedForms(self.user)
-        project_with_repeat_field = self._create_test_projects('Project-with-repeat-'
+        project_with_repeat_field = self._create_test_projects_and_delete_teardown('Project-with-repeat-'
                                                                                + self.random_project_name, self.REPEAT)
-        project_repeat_parent = self._create_test_projects('Project-repeat-parent'
+        project_repeat_parent = self._create_test_projects_and_delete_teardown('Project-repeat-parent'
                                                                                + self.random_project_name, self.REPEAT_PARENT)
-        correlated_forms.relate_forms(project_repeat_parent, project_with_repeat_field, 'Child-with-repeat')
+        correlated_forms.relate_parent_and_child_forms(project_repeat_parent, project_with_repeat_field, 'Child-with-repeat')
 
         updated_child_project = Project.get(self.dbm, project_with_repeat_field)
 
@@ -111,8 +131,9 @@ class TestCorrelatedXlsForms(unittest.TestCase):
         root = ET.fromstring(source_xml_str.encode('utf-8'))
         [self.assertTrue('true()', r.attrib['readonly']) for r in root.iterfind(bind_tag)]
 
-    def _create_test_projects(self, prj_name, xlxform):
+    def _create_test_projects_and_delete_teardown(self, prj_name, xlxform):
         errors, xform, json_xform_data = XlsFormParser(xlxform, prj_name).parse()
         mangroveService = MangroveService(self.user, xform, json_xform_data, project_name=prj_name)
         project_id, form_code = mangroveService.create_project()
+        self.clean_projects.add(project_id)
         return project_id
