dce76e90f24fde368a8b658ad3b15d93ef2ab1c4
Prince M Jain <princemj@thoughtworks.com>
2015-03-09 12:35:46 +0700

    Prince | updating fab file for new branch.

--- datawinners/blue/fabfile.py
+++ datawinners/blue/fabfile.py
@@ -7,7 +7,7 @@ from fabric.state import env
 '''
 To update the dcsweb.twhosted with new code and restart server, run
 fab qa deploy #or
-fab ci deploy
+fab prod deploy
 from $HOME/workspace/datawinners/datawinners/blue
 
 '''
@@ -20,8 +20,8 @@ MANGROVE_CODE = '/home/mangrover/virtual_env/datawinners/src/mangrove'
 #DW_BRANCH = 'develop-dcs-dw'
 #MANGROVE_BRANCH = 'develop-dcs'
 
-DW_BRANCH = 'merged-dw'
-MANGROVE_BRANCH = 'merged-mangrove'
+DW_BRANCH = 'develop'
+MANGROVE_BRANCH = 'develop'
 
 def deploy():
     if confirm('Continue to update the application code?'):
@@ -30,18 +30,18 @@ def deploy():
         restart_uwsgi()
 
 def qa():
-    env.hosts = ['172.18.29.2']
-
-def ci():
     env.hosts = ['172.18.29.3']
+
+def prod():
+    env.hosts = ['172.18.29.2']
 
 def deploy_mangrove():
     with cd(MANGROVE_CODE):
-        sudo('git pull --rebase team ' + MANGROVE_BRANCH, user=MANGROVER)
+        sudo('git pull --rebase origin ' + MANGROVE_BRANCH, user=MANGROVER)
 
 def deploy_dcs():
     with cd(DCS):
-        sudo('git pull --rebase team ' + DW_BRANCH, user=MANGROVER)
+        sudo('git pull --rebase origin ' + DW_BRANCH, user=MANGROVER)
 
 def restart_uwsgi():
     sudo("service uwsgi restart")
